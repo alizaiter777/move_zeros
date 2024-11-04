a3a8c9edd2574ded3a66715e8f33687b123f26a2
alizaiter777 <alizaiter777@gmail.com>
2024-11-04 20:31:43 +0200

    generate lists

--- exc.py
+++ exc.py
@@ -3,8 +3,9 @@
 ##using split function to split user input according to space
 #using map to convert each string to integer after splitting
 numbers= list(map(int, input("Enter list of integers: ").split()))
-
-
+#generate lists
+zeros=[]
+x=[]
 
 
 
